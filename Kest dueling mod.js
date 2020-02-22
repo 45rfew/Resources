5789e031f964fd3d3248457384584ad1386d562c
Money <48343592+45rfew@users.noreply.github.com>
2020-02-22 15:59:48 +1100

    Update Kest dueling mod.js

--- Kest dueling mod.js
+++ Kest dueling mod.js
@@ -1,7 +1,7 @@
 let strafe = false;
 //Set strafe to true if you want strafe and false if not
 const alien_array = [
-  {code:19,level:0}
+  {code:19,level:1}
 ];
 //add alien codes and their specified level the this arrary, if wanting 
 //to spawn those aliens
@@ -50,18 +50,31 @@ hide => {
     ship.hideInstructor();
   }
 };
+
+findPlayerByName = function(name){
+	for (let i=0; i<game.ships.length; i++){
+		if (game.ships[i].name == name){
+		  return game.ships[i];
+		}
+	}
+	return null;
+};
+
+//findPlayerByName(name).gameover({"":""});
+		
 //eg: hide() into the console  
 //Hides the instructor for all players
  
 //Don't change anything below this line unless you know what you're doing kay
 
-playerList => {
+playerList = function(){
   echo("\nList of players and their IDs:");
   for (let i=0; i<game.ships.length; i++){ 
     echo(i+": "+game.ships[i].name);  
   } 
   echo("\n");
 };
+
 
 var dragon = {
   id: "dragon",
@@ -241,10 +254,13 @@ this.options = {
 };
  
 var ship_list = [
-  [101,103,201,202,203,204,301,302,303,304,305,306,307,401,402,403,404,405,406,407,408,409,501,502,503,504,505,506,509,507,508,509,510,511,512,601,602,603,609,604,605,606,607,608,609,610,611,612,613,614,615,701,702,703,704,705,706,707]
+  [101,103,201,202,203,204,301,302,303,304,305,306,307,401,402,403,404,405,406,407,408,409,501,502,503,504,505,506,507,508,509,510,511,512,601,602,603,609,604,605,606,607,608,609,610,611,612,613,614,615,701,702,703,704,705,706,707]
 ];
  
 function tick(game){
+  if (game.step % 300 === 0){
+    //findPlayerByName("").gameover({"":""});
+  }
   for (let i=0; i<game.ships.length; i++){  
     var ship = game.ships[i];
     if (game.step % 60 === 0){
