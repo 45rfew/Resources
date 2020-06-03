d2b7fefbcd6caf4b17caad0d36de586648df2f90
Money <48343592+45rfew@users.noreply.github.com>
2020-06-03 18:36:29 +1000

    Project between L.gaming, Nex, Kirito, Bhpsngum and me

--- TDM.js
+++ TDM.js
@@ -1,3 +1,60 @@
+colors=[0,180];
+
+killstats = {
+  id: "info",
+  position: [2.5,29,60,100],
+  clickable: false,
+  visible: true,
+  components: []
+};
+
+var scoreboard = {
+  id:"scoreboard",
+  visible: true,
+  components: []
+};
+
+function showkills (game,event){
+  let s,defclr="#FFFFFF",pln={text:event.ship.name,color:`hsla(${colors[event.ship.team]},100%,50%,1)`};
+  if (Object.is(event.killer,null)) 
+  s= [
+    pln,
+    {text:"committed suicide",color:defclr},
+    {text:"",color:defclr}
+  ]; 
+  else
+  s= [
+    {text:event.killer.name,color:`hsla(${colors[event.killer.team]},100%,50%,1)`},
+    {text:"🗡️",color:defclr},
+    pln
+  ];
+  let size=0,line=killstats.components.length/3;
+  for (let i=0;i<s.length;i++){
+    let text = " "+s[i].text+" ";
+    killstats.components.push(
+      {type:"text",position:[size,(line+1)*5,text.length,20],value:text,color:s[i].color}
+    );
+    size+=text.length;
+  }
+  console.log(killstats);
+  for (let ship of game.ships) ship.setUIComponent(killstats);
+}
+
+var redpoints = 0, bluepoints = 0;
+function update(game){
+  for (let ship of game.ships){
+    ship.setUIComponent({
+      id: "points",
+      position: [40,6,22,20],
+      visible: true,
+      components: [
+        {type: "text",position:[-20,0,80,33],value:`Red ${redpoints}`,color:"#ff0000"},
+        {type: "text",position:[4,0,80,33],value:`-`,color:"#fff"},
+        {type: "text",position:[30,0,80,33],value:`${bluepoints} Blue`,color:"#0000ff"},
+      ]
+    });
+  }  
+}
 
 var map = 
 "99999999999999999999999999999999999999999999999999\n"+
@@ -51,9 +108,27 @@ var map =
 "99999999999999999999999999999999999999999999999999\n"+
 "99999999999999999999999999999999999999999999999999";
 
+this.options = {
+  map_name: "TDM",
+  custom_map: map,
+  map_size: 50,
+  max_players: 20,
+  radar_zoom: 2,
+  survival_level: 8,
+  reset_tree: false,
+  root_mode: "",
+  asteroids_strength: 5,
+  crystal_value: 0,
+  friendly_colors: 2,
+  lives: 0,
+  starting_ship: 801,
+  weapons_store: false,
+  soundtrack: "argon.mp3"
+};
+
 splitIntoTeams = () => {
   let teams = [
-    {hue:180,x:-200,y:0}, {hue:0,x:200,y:0}
+    {hue:0,x:200,y:0}, {hue:180,x:-200,y:0} 
   ];
   for(let i = 0; i<game.ships.length; i++){
     let ship = game.ships[i];
@@ -64,7 +139,7 @@ splitIntoTeams = () => {
 
 onspawn = (ship) => {
   var teams = [
-    {hue:180,x:-200,y:0}, {hue:0,x:200,y:0}
+    {hue:0,x:200,y:0}, {hue:180,x:-200,y:0} 
   ];
   for(var i = 0; i<game.ships.length; i++) var team = i%2;  
   ship.set({hue:teams[team].hue,team:team,x:teams[team].x,y:teams[team].y});
@@ -74,28 +149,26 @@ spawnSecondary = () => {
   var range = 10;
   var x = range * (Math.random()*2-1);
   var y = range * (Math.random()*2-1);
-  var options = [10, 11, 12, 20, 21, 90, 91]; //secondary codes
+  var options = [10,11,/*12,*/20,21,90,91];
   var spawnCode = options[Math.floor(Math.random()*options.length)];
   game.addCollectible({code:spawnCode,x:x,y:y});
 };
 
-this.options = {
-  map_name: "TDM",
-  map_size: 50,
-  max_players: 20,
-  custom_map: map,
-  radar_zoom: 2,
-  survival_level: 8,
-  reset_tree: false,
-  root_mode: "",
-  asteroids_strength: 5,
-  crystal_value: 0,
-  friendly_colors: 2,
-  lives: 0,
-  starting_ship: 801,
-};
+function distance(x, y){
+  return Math.sqrt(x*x+y*y);
+}
 
-this.tick = function(game){
+function checkteambase(){
+  for (let ship of game.ships){
+    if (ship.team === 0){
+      if (ship.x > 185 && ship.x < 240 && Math.abs(ship.y) < 35) echo("team red base");
+    } else if (ship.team === 1){
+      if (ship.x > -185 && ship.x < -240 && Math.abs(ship.y) < 35) echo("team blue base");
+    }  
+  }
+}
+
+this.tick = function (game){
   if (game.step % 30 === 0){
     for (let ship of game.ships){
       if (!ship.custom.init){
@@ -103,23 +176,35 @@ this.tick = function(game){
         onspawn(ship);
       }
     }
+    update(game);
   }
-  if (game.step % 60*15 === 0){
-    spawnSecondary();
+  if (game.step % 300 === 0){
+    killstats.components.splice(0,3);
+    for (let i=0;i<killstats.components.length;i++) killstats.components[i].position[1]-=5;
+    for (let ship of game.ships) ship.setUIComponent(killstats);
   }
+  if (game.step % 700 === 0) spawnSecondary();
 };
 
-this.event = function(event,game) {
+this.event = function (event,game){
   switch (event.name){
     case "ship_spawned":
       var ship = event.ship;
       var ship_level = Math.trunc(ship.type / 100);
       if (ship !== null){
-        if (ship.team === 0) ship.set({x:-200,y:0,crystals:((ship_level||0)**2)*20});  
+        if (ship.team === 0) ship.set({x:-200,y:0,crystals:((ship_level||0)**2)*20/2});  
           else 
-        if (ship.team === 1) ship.set({x:200,y:0,crystals:((ship_level||0)**2)*20});
+        if (ship.team === 1) ship.set({x:200,y:0,crystals:((ship_level||0)**2)*20/2});
+      }
+    break;    
+    case "ship_destroyed":
+      showkills(game,event);
+      console.log(event);
+      if (event.killer.team === 0){
+        bluepoints++;
+      } else if (event.killer.team === 1){
+        redpoints++;
       }
     break;
   }
 };
-
