2b1a0f7baea83d578dfab256cc9e95c43d1ef107
Money <48343592+45rfew@users.noreply.github.com>
2020-06-28 19:16:07 +1000

    Update Alien Defense.js

--- Alien Defense.js
+++ Alien Defense.js
@@ -96,7 +96,7 @@ this.options = {
   station_size: 1
 };
  
-function tick(game){     
+function tick(game){  
   if (game.step % 3600 === 0){
     if (game.ships.length === 1||2){
       rip += 1;
@@ -122,6 +122,24 @@ function tick(game){
         ship.custom.init = true;
         joinmessage(ship);
         basetimer(game);
+        game.configImageUI({
+          id: "very important",
+          visible: true,
+          clickable: false,
+          url: "https://raw.githubusercontent.com/Bhpsngum/img-src/master/yeet.jpg",
+          position: {x:5,y:30},
+          scale: {x:1,y:1.5},
+          opacity: 1
+        },ship.setUIComponent);
+        game.configImageUI({
+          id: "very important2",
+          visible: true,
+          clickable: false,
+          url: "https://raw.githubusercontent.com/Bhpsngum/img-src/master/yeet2.jpg",
+          position: {x:8,y:30},
+          scale: {x:1,y:1.5},
+          opacity: 1
+        },ship.setUIComponent);        
       }
     }
     if (rip > 79){
@@ -236,15 +254,15 @@ function joinmessage(ship){
 game.configImageUI = function(opt, handler){
   let image = {
     id: opt.id,
-    visible: (opt.visible != void 0)?opt.visible:true,
-    clickable: (opt.clickable != void 0)?opt.clickable:false,
+    visible: opt.visible,
+    clickable: opt.clickable,
     components: []
   };
   if (opt.url){
-    let img = $("#img_test")[0];
+    let img = $("#"+opt.id)[0];
     if (!img){
       img = document.createElement("img");
-      img.setAttribute("id","img_test");
+      img.setAttribute("id",opt.id);
       img.setAttribute("style","display:none");
       img.crossOrigin = "anonymous";
       $('body').append(img);
@@ -254,16 +272,30 @@ game.configImageUI = function(opt, handler){
       this.canvas.width = img.width;
       this.canvas.height = img.height;
       this.canvas.getContext('2d').drawImage(this, 0, 0, this.width, this.height);
-      for (let i=0;i<this.height;i++)
+      let mx,my,h=100/this.height,w=100/this.width,d=[],pos=opt.position||{x:0,y:0},scale = opt.scale||{x:1,y:1};
+      function round(num){
+        return parseFloat(num.toFixed(2));
+      }
+      if (["scoreboard","radar_background"].indexOf(opt.id) != -1){
+        d=[0,0];
+        mx=(pos.x||0)/100;
+        my=(pos.y||0)/100;
+      } else {
+        d = [pos.x||0,pos.y||0];
+        mx=0;
+        my=0;
+      }
+      image.position = [d,[round(w*(scale.x||1)),round(h*(scale.y||1))]].flat();
+      for (let i=0;i<this.height;i++){
         for (let j=0;j<this.width;j++){
-          let data = $("#img_test")[0].canvas.getContext('2d').getImageData(j,i,1,1).data;
-          image.components.push({type:"box",position:[j*0.5,i,0.5,1],fill:`rgba(${data[0]},${data[1]},${data[2]},${data[3]/255*(opt.opacity||1)})`});
+          let data = img.canvas.getContext('2d').getImageData(j,i,1,1).data;
+          image.components.push({type:"box",position:[round(w*j+mx),round(h*i+my),round(w*1.5),round(h*1.5)],fill:`rgba(${data[0]},${data[1]},${data[2]},${data[3]/255*(opt.opacity||1)})`});
         }
-      image.position = [opt.x||0,opt.y||0,this.width/2,this.height/2];
+      }
       (typeof handler == "function") && handler(image);
     };
     img.onerror = function(){throw new Error("Failed to fetch the image")};
     img.setAttribute("src",opt.url);
   }
-  else throw new Error("No Image Url detected!");
+  else throw new Error("No Image Url detected");
 };
